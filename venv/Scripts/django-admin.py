9844727562455628eeb767589efb827200ea6b4b
Oussama MZ <oussamamazzouz@hotmail.com>
2020-05-22 12:20:34 +0000

    Adding Auth With JWT and Users Get Link

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Oussa\Desktop\2020 autoFormation\DjangoRestFramwork\miniProjet\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
